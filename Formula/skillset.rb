ba00c3bb969d980e030bf3deab5a14de41b07670
Matt Galligan <matt.galligan@gmail.com>
2026-09-09 19:23:37 +0000

    skillset: add 0.26.0 bottle.

--- Formula/skillset.rb
+++ Formula/skillset.rb
@@ -4,6 +4,12 @@ class Skillset < Formula
   url "https://github.com/outfitter-dev/skillset/releases/download/v0.26.0/skillset-v0.26.0-darwin-arm64.tar.gz"
   sha256 "72199a67ffb8b881e17dba622ff1b88af2d2e5fcd959a9867e75d4a352314537"
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/outfitter-dev/homebrew-tap/releases/download/skillset-0.26.0"
+    sha256                               arm64_tahoe: "efb07af5607894c2b38b07c903f1d82e84c3909c704328cb1f4f9cc924ed93d3"
+    sha256 cellar: :any_skip_relocation, sequoia:     "4a3dd351957850ffb8a36299311db0c0e1cbe3f3f45370af3f5f70b6cc907c30"
+  end
   depends_on :macos
 
   on_macos do
