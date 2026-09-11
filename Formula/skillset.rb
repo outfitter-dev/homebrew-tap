1bac10825d12321769548e73a29e461570c31e9d
galligan <25037981+galligan@users.noreply.github.com>
2026-09-11 16:24:08 +0000

    chore(skillset): update formula to v0.26.1
    Closes #27.
    
    Signed-off-by: Matt Galligan <matt.galligan@gmail.com>

--- Formula/skillset.rb
+++ Formula/skillset.rb
@@ -1,21 +1,15 @@
 class Skillset < Formula
   desc "Source-first compiler for provider-native agent loadouts"
   homepage "https://github.com/outfitter-dev/skillset"
-  url "https://github.com/outfitter-dev/skillset/releases/download/v0.26.0/skillset-v0.26.0-darwin-arm64.tar.gz"
-  sha256 "72199a67ffb8b881e17dba622ff1b88af2d2e5fcd959a9867e75d4a352314537"
+  url "https://github.com/outfitter-dev/skillset/releases/download/v0.26.1/skillset-v0.26.1-darwin-arm64.tar.gz"
+  sha256 "6a4540121c16486b8f52e2fb3809077e7292915f52e6a8172e4449d10de70251"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/outfitter-dev/homebrew-tap/releases/download/skillset-0.26.0"
-    sha256                               arm64_tahoe: "efb07af5607894c2b38b07c903f1d82e84c3909c704328cb1f4f9cc924ed93d3"
-    sha256 cellar: :any_skip_relocation, sequoia:     "4a3dd351957850ffb8a36299311db0c0e1cbe3f3f45370af3f5f70b6cc907c30"
-  end
   depends_on :macos
 
   on_macos do
     on_intel do
-      url "https://github.com/outfitter-dev/skillset/releases/download/v0.26.0/skillset-v0.26.0-darwin-x64.tar.gz"
-      sha256 "abcbb11747fa2eeaeb943b97da02879e649a23f14f0f47f5a8c60bd622c8035b"
+      url "https://github.com/outfitter-dev/skillset/releases/download/v0.26.1/skillset-v0.26.1-darwin-x64.tar.gz"
+      sha256 "146e662505c15f9f0d77c4d8ae576d3aa6a3198a70d1c7e5b972f078de38bfe4"
     end
   end
 
